d9d284f14097c934de199441e01106a9ccd413ec
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2021-12-30 21:46:36 -0600

    fixed skip and money mechanics

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,28 +8,27 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function (enemyName) {
-  while (enemyHealth > 0) {
+  while (playerHealth > 0 && enemyHealth > 0) {
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-    var promptFight = window.prompt(
-      "Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose."
-    );
+    if (promptFight === "skip" || promptFight === "SKIP") {
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+  
+        if (confirmSkip) {
+          window.alert(playerName + " has decided to skip this fight. Goodbye!");
+          playerMoney = playerMoney - 10;
+          console.log("playerMoney", playerMoney);
+          break;
+        }
+    }
 
-    if (promptFight === "fight" || promptFight === "FIGHT" || promptFight === "f") {
-      enemyHealth = enemyHealth - playerAttack;
-
-      console.log(
-        playerName +
-          " attacked " +
-          enemyName +
-          ". " +
-          enemyName +
-          " now has " +
-          enemyHealth +
-          " health remaining."
-      );
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
 
       if (enemyHealth <= 0) {
         window.alert(enemyName + " has died.");
+        playerMoney = playerMoney + 20;
+        break;
       } else {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
       }
@@ -49,24 +48,13 @@ var fight = function (enemyName) {
 
       if (playerHealth <= 0) {
         window.alert(playerName + " has died.");
+        break;
       } else {
         window.alert(
           playerName + " still has " + playerHealth + " health left."
         );
       }
-    } else if (promptFight === "skip" || promptFight === "SKIP") {
-      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-      if (confirmSkip) {
-        window.alert(playerName + " has decided to skip this fight. Goodbye!");
-        playerMoney = playerMoney - 2;
-      } else {
-        fight();
-      }
-    } else {
-      window.alert("You need to choose a valid option. Try again!");
     }
-  }
 };
 
 for (var i = 0; i < enemyNames.length; i++) {
